b72e4bfee45f5e390a0eccd40b695f063ef06a10
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-21 09:20:58 -0700

    Relaid out file to be the Ruby standard of 2 spaces indent and no tabs. #1697

--- lib/shipping/usps.rb
+++ lib/shipping/usps.rb
@@ -5,22 +5,22 @@
 module Shipping
   
 
-	class USPS < Base
+  class USPS < Base
 
-		API_VERSION = "1.0001"
-		
-		
-		def price
-			@required = [:zip, :country, :sender_zip, :weight]
-			@required += [:usps_account]
-			@country ||= 'US'
-			@data = String.new			
-			
+    API_VERSION = "1.0001"
+    
+    
+    def price
+      @required = [:zip, :country, :sender_zip, :weight]
+      @required += [:usps_account]
+      @country ||= 'US'
+      @data = String.new            
+      
       shipping_pounds = @weight.floor
       shipping_ounces = (16 * (@weight - shipping_pounds)).round
-			
-			b = Builder::XmlMarkup.new(:target => @data)
-			b.instruct!
+      
+      b = Builder::XmlMarkup.new(:target => @data)
+      b.instruct!
       b.RateV3Request('USERID'=>@usps_account){ |b|
         b.Package('ID'=>'1ST'){ |b|
           b.Service ServiceTypes[@service_type] || ServiceTypes['priority']
@@ -33,24 +33,24 @@ module Shipping
       }
       get_get_response 'http://production.shippingapis.com/shippingapi.dll'
       r = REXML::XPath.first(@response, "//RateV3Response/Package/Postage/Rate").text.to_f
-			return r
-		rescue
-		  raise ShippingError, get_error
-		end
-		
-		private
-		
-		def get_error
-			code = REXML::XPath.first(@response, "//Error/Number").text
-			message = REXML::XPath.first(@response, "//Error/Description").text
+      return r
+    rescue
+      raise ShippingError, get_error
+    end
+    
+    private
+    
+    def get_error
+      code = REXML::XPath.first(@response, "//Error/Number").text
+      message = REXML::XPath.first(@response, "//Error/Description").text
       debugger
-			return "Error #{code}: #{message} \n Sent: #{@req}"
-		end
-		
-		ServiceTypes = {
-			"priority" => "PRIORITY",
-			"express" => "EXPRESS"
-		}
-		
-	end
+      return "Error #{code}: #{message} \n Sent: #{@req}"
+    end
+    
+    ServiceTypes = {
+      "priority" => "PRIORITY",
+      "express" => "EXPRESS"
+    }
+    
+  end
 end
